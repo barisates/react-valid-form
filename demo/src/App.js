5d88a7de047768d70b148e77ae30f809224e63a6
bates <bates@trabilisim.com>
2019-09-20 17:02:43 +0300

    demo test development

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,265 +1,106 @@
 import React, { Component } from 'react';
+import ValidForm from 'react-valid-form';
 import './App.css';
-
-
-const warnings = {
-  required: "This field is required.",
-  numeric: "This field must be numeric.",
-  min: "",
-  max: "",
-  minlength: "This field must be at least 3 characters.",
-  maxlength: "This field must not exceed 3 characters."
-}
-
-const rules = {
-  required: {
-    valid: (value) => {
-      return (value !== null && value !== undefined && value !== "")
-    },
-    warning: () => "This field is required."
-  },
-  number: {
-    valid: (value) => {
-      return (!isNaN(value))
-    },
-    warning: () => "This field must be numeric."
-  },
-  min: {
-    valid: (value, min) => {
-      return (value != null && value >= min)
-    },
-    warning: (min) => `The maximum value of this field can be ${min}.`
-  },
-  max: {
-    valid: (value, max) => {
-      return (value != null && value <= max)
-    },
-    warning: (max) => `The maximum value of this field can be ${max}.`
-  },
-  minlength: {
-    valid: (value, length) => {
-      return (value != null && value.toString().length >= length)
-    },
-    warning: (length) => `This field must be at least ${length} characters.`
-  },
-  maxlength: {
-    valid: (value, length) => {
-      return (value != null && value.toString().length <= length)
-    },
-    warning: (length) => `This field must not exceed ${length} characters.`
-  },
-  email: {
-    valid: (value) => {
-      const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return regEx.test(String(value).toLowerCase());
-    },
-    warning: () => "Enter a valid email address."
-  },
-  // OWN
-  keys: () => (Object.keys(rules).map(item => item.toLowerCase()))
-};
-
-const tags = ["INPUT", "SELECT", "TEXTAREA"]
-
-const utilities = {
-  isElement: (tag) => (tags.includes(tag)),
-  elements: (formRef) => (Array.from(formRef.current.elements).filter(filter => utilities.isElement(filter.tagName))),
-  validation: (element) => {
-
-    console.log(element.attributes, "-----------------")
-
-    var typ = document.createAttribute(element.type);
-    typ.value = "";
-    element.attributes.setNamedItem(typ);
-
-    console.log(element.attributes, "-----------------")
-    let validates = Object.values(element.attributes).filter(filter => rules.keys().includes(filter.name.toLowerCase()))
-
-
-
-    return validates;
-  },
-  invalid: (element, warning) => {
-
-    element.classList.add("invalid");
-
-    let span = element.parentNode.getElementsByClassName("warning-invalid")[0];
-
-    if (!span) {
-      span = document.createElement('span');
-      span.className = "warning-invalid";
-    }
-
-    span.innerHTML = warning;
-
-    element.parentNode.appendChild(span);
-
-  },
-  valid: (element) => {
-
-    element.classList.remove("invalid");
-
-    let span = element.parentNode.getElementsByClassName("warning-invalid")[0];
-
-    if (span)
-      element.parentNode.removeChild(span);
-
-  }
-}
-
-export class ValidForm extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      form: {
-
-      }
-    };
-
-    this.formRef = React.createRef();
-    this.formElements = [];
-
-  }
-  componentDidMount() {
-
-    this.formElements = utilities.elements(this.formRef);
-
-    let form = {};
-
-    this.formElements.forEach(element => {
-      form[element.name] = null;
-    });
-
-    this.setState({
-      form: form
-    });
-
-  }
-  onChange(e) {
-
-    const element = e.target;
-
-    if (utilities.isElement(element.tagName)) {
-      this.setState(prevState => ({
-        form: { ...prevState.form, [element.name]: element.value }
-      }))
-    };
-
-  }
-  onSubmit(e) {
-    e.preventDefault();
-
-    let valid = true;
-
-    this.formElements.forEach(element => {
-
-      let validation = utilities.validation(element);
-
-      for (let validate of validation) {
-
-        let elementValue = this.state.form[element.name];
-
-        const rule = rules[validate.name];
-
-        if (!rule.valid(elementValue, validate.value)) {
-
-          utilities.invalid(element, rule.warning(validate.value));
-
-          if (valid)
-            element.focus();
-
-          valid = false;
-
-          break;
-        }
-
-        utilities.valid(element);
-      }
-
-    });
-
-    if (this.props.onSubmit)
-      this.props.onSubmit(e.target, this.state.form, valid);
-
-    return false;
-  }
-  render() {
-    const { onSubmit, ...props } = this.props;
-
-    return (
-      <form {...props} noValidate ref={this.formRef} onChange={(e) => this.onChange(e)} onSubmit={(e) => this.onSubmit(e)}>
-        {this.props.children}
-      </form>
-    )
-  }
-}
 
 
 // DEMO
 class App extends Component {
-  constructor() {
-    super()
-  }
   onSubmit(form, data, valid) {
-
-    console.log(form, data, valid, " ------ form");
-  }
-  onResponse(json) {
-    console.log(json, " ------ json");
-  }
-  onCatch(error) {
-    console.log(error, " ------ error");
+    console.log(form, " | FORM");
+    console.log(data, " | DATA");
+    console.log(valid, " | VALID");
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <ValidForm>
-            <button type="submit">Send</button>
-          </ValidForm>
-          <ValidForm action="https://httpbin.org/post" method="post" onSubmit={(form, data, valid) => this.onSubmit(form, data, valid)}>
+          <ValidForm onSubmit={(form, data, valid) => this.onSubmit(form, data, valid)}>
             <div className="row">
               <div className="col-md-4 offset-md-4">
-                <h5 className="mb-3">Create Account</h5>
-                <div className="row">
+                <h5 className="mb-3">Validation Examples</h5>
+
+                <div className="row pt-3">
                   <div className="form-group col-md-6">
-                    <label htmlFor="firstname">First name</label>
-                    <input type="text" className="form-control" id="firstname" name="firstname" placeholder="Enter first name" required number="true" /*maxLength="10" minLength="5" max="4"*/ />
+                    <label htmlFor="string">Text Max-Min Length <small>[minLength="3", maxLength="50"]</small></label>
+                    <input type="text" className="form-control" id="string" name="string" placeholder="Enter text" minLength="3" maxLength="50" />
                   </div>
                   <div className="form-group col-md-6">
-                    <label htmlFor="lastname">Last name</label>
-                    <input type="number" className="form-control" id="lastname" name="lastname" placeholder="Enter last name" required minLength="3" />
+                    <label htmlFor="onlyNumber">Number Max-Min Value <small>[number="true", min="3", max="50"]</small></label>
+                    <input type="text" className="form-control" id="onlyNumber" name="onlyNumber" placeholder="Enter only number" number="true" min="3" max="50" />
                   </div>
                 </div>
-                <div className="row">
-                  <div className="form-group col-md-12">
-                    <label htmlFor="email">Email address</label>
+
+                <div className="row pt-3">
+                  <div className="form-group col-md-6">
+                    <label htmlFor="email">Email address <small>[type="email"]</small></label>
                     <input type="email" className="form-control" id="email" name="email" placeholder="Enter email" autoComplete="username email" />
                   </div>
-                </div>
-                <div className="row">
-                  <div className="form-group col-md-12">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" id="password" name="password" placeholder="Password" autoComplete="current-password" minLength="6" />
+                  <div className="form-group col-md-6">
+                    <label htmlFor="url">Url <small>[type="url"]</small></label>
+                    <input type="url" className="form-control" id="url" name="url" placeholder="Enter url" />
                   </div>
                 </div>
-                <div className="row">
+
+                <div className="row pt-3">
                   <div className="form-group col-md-6">
-                    <label htmlFor="gender">Gender</label>
-                    <select className="form-control" name="gender" id="gender" defaultValue="">
-                      <option value="" disabled> Select</option>
-                      <option value="male">Male</option>
-                      <option value="female">Female</option>
+                    <label htmlFor="password">Password <small>[confirm="passwordConfirm"]</small></label>
+                    <input type="password" className="form-control" id="password" name="password" placeholder="Password" autoComplete="current-password" confirm="passwordConfirm" />
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label htmlFor="password">Password Confirm <small>[required]</small></label>
+                    <input type="password" className="form-control" id="passwordConfirm" name="passwordConfirm" placeholder="Password Confirm" autoComplete="current-password" required />
+                  </div>
+                </div>
+                <div className="row pt-3">
+                  <div className="form-group col-md-6">
+                    <label htmlFor="regexp">Regexp <small>[regexp="\d"]</small></label>
+                    <input type="text" className="form-control" id="regexp" name="regexp" placeholder="Enter only number" regexp="\d" />
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label htmlFor="letters">Only Letters <small>[required, letters="true"]</small></label>
+                    <input type="text" className="form-control" id="letters" name="letters" placeholder="Enter only letters" required letters="true" />
+                  </div>
+                </div>
+
+                <div className="row pt-3">
+                  <div className="form-group col-md-6">
+                    <label htmlFor="select">Select Required <small>[required]</small></label>
+                    <select className="form-control" name="select" id="select" defaultValue="" required>
+                      <option value=""> Select</option>
+                      <option value="option1">Option 1</option>
+                      <option value="option2">Option 2</option>
+                      <option value="option3">Option 3</option>
                     </select>
                   </div>
+                  <div className="form-group col-md-6">
+                    <label htmlFor="textarea">Textarea Required <small>[required, maxLength="255"]</small></label>
+                    <textarea id="textarea" name="textarea" class="form-control" rows="1" placeholder="Enter text" required maxLength="255"></textarea>
+                  </div>
                 </div>
-                <div className="form-group form-check">
-                  <input type="checkbox" className="form-check-input" id="termsofservice" name="termsofservice" />
-                  <label className="form-check-label" htmlFor="termsofservice">I accept the <a href="#terms">Terms of Service.
-                  </a></label>
+                <div className="row pt-3">
+                  <div className="form-group form-check col-md-6">
+                    <label htmlFor="country">Checkbox Required <small>[required]</small></label>
+                    <div className="custom-control custom-checkbox">
+                      <input type="checkbox" className="custom-control-input" id="checkbox" name="checkbox" required />
+                      <label className="custom-control-label" htmlFor="checkbox">I accept the <a href="#terms">Terms of Service.</a></label>
+                    </div>
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label htmlFor="gender">Radio Required <small>[required]</small></label>
+                    <div className="ml-1">
+                      <div className="custom-control custom-radio custom-control-inline">
+                        <input type="radio" id="radio1" name="radio" className="custom-control-input" />
+                        <label className="custom-control-label" htmlFor="genderMale">Radio 1</label>
+                      </div>
+                      <div className="custom-control custom-radio custom-control-inline">
+                        <input type="radio" id="radio2" name="radio" className="custom-control-input" required />
+                        <label className="custom-control-label" htmlFor="genderFemale">Radio 2</label>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit Form Fetch</button>
+
+                <button type="submit" className="btn btn-primary">Submit Form</button>
               </div>
             </div>
           </ValidForm>
--- src/index.js
+++ src/index.js
@@ -1,42 +1,248 @@
 import React, { Component } from 'react';
-import '/index.css';
+import './index.css';
 
-const warnings = {
-    required: ""
+/* Utilities */
+const config = {
+  warning: {
+    invalid: "invalid-field",
+    field: "invalid-field-warning"
+  }
 }
 
-const rules = {
-    required: (value) => {
-        return (value !== null && value !== undefined && value !== "")
+const utilities = {
+  formFields: (tag) => {
+    // form fields tag
+    const tags = ["INPUT", "SELECT", "TEXTAREA"]
+
+    return tags.includes(tag);
+  },
+  elements: (formRef) => {
+
+    // get form fields from Ref
+    return Array.from(formRef.current.elements).filter(
+      filter => utilities.formFields(filter.tagName)
+    );
+
+  },
+  validation: (element, rules, warnings) => {
+
+    // add form field type to attributes.
+    element.attributes.setNamedItem(document.createAttribute(element.type));
+
+    // filter validation rules from form field attributes.
+    return Object.values(element.attributes)
+      .filter(
+        filter => rules.keys().includes(filter.name.toLowerCase())
+      )
+      .map(
+        map => ({
+          name: map.name,
+          value: map.value,
+          valid: rules[map.name],
+          warning: warnings[map.name]
+        })
+      );
+
+  },
+  invalid: (element, warning) => {
+
+    // get warning field
+    let span = element.parentNode.getElementsByClassName(config.warning.field)[0];
+
+    // create warning field if not exist
+    if (!span) {
+      span = document.createElement('span');
+      span.className = config.warning.field;
     }
+
+    span.innerHTML = warning;
+    element.parentNode.appendChild(span);
+
+    // add invalid class
+    element.classList.add(config.warning.invalid);
+  },
+  valid: (element) => {
+
+    // remove warning field if exist
+    const span = element.parentNode.getElementsByClassName(config.warning.field)[0];
+
+    if (span)
+      element.parentNode.removeChild(span);
+
+    // remove invalid class
+    element.classList.remove(config.warning.invalid);
+  }
+}
+
+/* Warnings */
+export const Warnings = {
+  required: () => "This field is required.",
+  number: () => "This field must be numeric.",
+  alphanumeric: () => "This field must be alphanumeric.",
+  letters: () => "This field must be only letters.",
+  min: (min) => `The minimum value of this field can be ${min}.`,
+  max: (max) => `The maximum value of this field can be ${max}.`,
+  minlength: (length) => `This field must be at least ${length} characters.`,
+  maxlength: (length) => `This field must not exceed ${length} characters.`,
+  email: () => "Enter a valid email address.",
+  url: () => "Enter a valid web address.",
+  confirm: () => "This field has the same value as the confirm field.",
+  regexp: () => "This field must match the pattern."
+}
+
+/* Rules */
+export const Rules = {
+  required: (value) => {
+    return (value !== null && value !== undefined && value !== "")
+
+  },
+  number: (value) => {
+    return (!isNaN(value))
+  },
+  alphanumeric: (value) => {
+    const regEx = /^[a-zşŞğĞüÜçÇöÖıİ0-9]+$/i;
+    return regEx.test(String(value).toLowerCase());
+  },
+  letters: (value) => {
+    const regEx = /^[a-zşŞğĞüÜçÇöÖıİ]+$/i;
+    return regEx.test(String(value).toLowerCase());
+  },
+  min: (value, min) => {
+    return (value != null && Number(value) >= min)
+  },
+  max: (value, max) => {
+    return (value != null && Number(value) <= max)
+  },
+  minlength: (value, length) => {
+    return (value != null && value.toString().length >= length)
+
+  },
+  maxlength: (value, length) => {
+    return (value != null && value.toString().length <= length)
+
+  },
+  email: (value) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regEx.test(String(value).toLowerCase());
+  },
+  url: (value) => {
+    const regEx = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+    return regEx.test(String(value).toLowerCase());
+  },
+  confirm: (value, repeatId) => {
+    let element = document.getElementById(repeatId);
+
+    if (element)
+      return (element.value === value)
+
+    return false;
+  },
+  regexp: (value, exp) => {
+    const regEx = new RegExp(exp)
+    return regEx.test(String(value).toLowerCase());
+  },
+  // OWN
+  keys: () => (Object.keys(Rules).map(item => item.toLowerCase()))
 };
 
-const tags = ["INPUT", "SELECT", "TEXTAREA"]
 
 
-export class ValidForm extends Component {
-    constructor() {
-        super()
+export default class ValidForm extends Component {
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            form: {
+    this.state = {
+      form: {
 
-            }
-        };
+      }
+    };
 
-        this.formRef = React.createRef();
-    }
-    componentDidMount() {
-        
-    }
-    onChange(e) {
+    this.formRef = React.createRef();
+    this.formElements = [];
 
-    }
-    render() {
-        return (
-            <form ref={this.formRef} onChange={(e) => this.onChange(e)}>
-                {this.props.children}
-            </form>
-        )
-    }
+  }
+  componentDidMount() {
+    // get form fields from Ref
+    this.formElements = utilities.elements(this.formRef);
+
+    let form = {};
+
+    // set default null
+    this.formElements.forEach(element => {
+      form[element.name] = null;
+    });
+
+    this.setState({
+      form: form
+    });
+  }
+
+  onChange(e) {
+
+    const element = e.target;
+
+    // is it form field?
+    if (utilities.formFields(element.tagName)) {
+
+      // checkbox value
+      let value = (element.type === "checkbox" && !element.checked ? "" : element.value);
+
+      // set form state
+      this.setState(prevState => ({
+        form: { ...prevState.form, [element.name]: value }
+      }));
+
+    };
+
+  }
+  onSubmit(e) {
+    e.preventDefault();
+
+    // get form fields from Ref
+    this.formElements = utilities.elements(this.formRef);
+
+    let valid = true;
+
+    this.formElements.forEach(element => {
+
+      // get form field value
+      let elementValue = this.state.form[element.name];
+
+      // get form field validation rules
+      let validationRules = utilities.validation(element, Rules, Warnings);
+
+      for (let rule of validationRules) {
+
+        // check rule
+        if (!rule.valid(elementValue, rule.value)) {
+
+          // set field invalid
+          utilities.invalid(element, rule.warning(rule.value));
+
+          if (valid) element.focus();
+
+          valid = false;
+          break;
+        }
+
+        // set field valid
+        utilities.valid(element);
+      }
+
+    });
+
+    if (this.props.onSubmit)
+      this.props.onSubmit(e.target, this.state.form, valid);
+
+    return false;
+  }
+  render() {
+    const { onSubmit, onChange, ...props } = this.props;
+
+    return (
+      <form {...props} noValidate ref={this.formRef} onChange={(e) => this.onChange(e)} onSubmit={(e) => this.onSubmit(e)}>
+        {this.props.children}
+      </form>
+    )
+  }
 }
